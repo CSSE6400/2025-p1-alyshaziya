8a275148080924e3d308e6517790d9bc1f24ddec
alyshaziya <alyshaziya@icloud.com>
2025-03-03 21:03:46 +1000

    fix routes.py

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -20,7 +20,6 @@ def get_todos():
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
-
     return jsonify({
         "id": id,
         "title": "Watch CSSE6400 Lecture",
@@ -33,7 +32,6 @@ def get_todo(id):
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-
     return jsonify({
         "id": 1,
         "title": "Watch CSSE6400 Lecture",
@@ -42,7 +40,7 @@ def create_todo():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-     })
+     }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
